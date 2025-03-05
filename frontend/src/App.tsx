37074054806b623c8e42fa419a6b37895b4d181a
Niels Kaars <nielskaars@gmail.com>
2025-03-05 13:59:47 +0100

    endpoint login/register added in backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <div className="w-full h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
           <Routes>
-            <Route path="/" element={<Register />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/chat" element={<Chat />} />
